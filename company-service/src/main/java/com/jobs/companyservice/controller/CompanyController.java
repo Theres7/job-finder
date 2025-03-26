0dc38cd0786b569391a166792a814f20732a6693
Theres Paul <therespaul77@gmail.com>
2025-03-26 19:05:32 +0530

    Company controller

---
+++ company-service/src/main/java/com/jobs/companyservice/controller/CompanyController.java
@@ -0,0 +1,56 @@
+package com.jobs.companyservice.controller;
+
+import com.jobs.companyservice.model.Company;
+import com.jobs.companyservice.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+@RequiredArgsConstructor
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+    @GetMapping
+    public ResponseEntity<List<Company>> getAllCompanies() {
+        List<Company> companies = companyService.getAllCompanies();
+        return new ResponseEntity<>(companies, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createCompany(@RequestBody Company company) {
+        companyService.saveCompany(company);
+        return new ResponseEntity<>(company.getName() + " is Created ", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable Long id) {
+        Company company = companyService.getCompanyById(id);
+        if (company != null) {
+            return new ResponseEntity<>(company, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> createCompany(@PathVariable("id") Long id, @RequestBody Company company) {
+        boolean updatedCompany = companyService.updateCompany(id, company);
+        if (updatedCompany) {
+            return new ResponseEntity<>("Company updated", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Company not updated", HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<String> deleteCompany(@PathVariable("id") Long id) {
+        boolean isDeleted = companyService.deleteCompany(id);
+        if (isDeleted) {
+            return new ResponseEntity<>("Company deleted!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Company not found!", HttpStatus.NOT_FOUND);
+    }
+}
