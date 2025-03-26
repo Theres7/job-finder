d7baad4ff52995daae2b30297ce0eb039df963b1
Theres Paul <therespaul77@gmail.com>
2025-03-26 18:59:39 +0530

    Adding Company model

---
+++ company-service/src/main/java/com/jobs/companyservice/model/Company.java
@@ -0,0 +1,22 @@
+package com.jobs.companyservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "companies")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String location;
+    private String contactDetails;
+}
