be37156f6fcad0a63220cf032e1ca06ff8a320a6
Theres Paul <therespaul77@gmail.com>
2025-03-28 12:18:50 +0530

    Update JobController.java

--- job-service/src/main/java/com/jobs/jobservice/controller/JobController.java
+++ job-service/src/main/java/com/jobs/jobservice/controller/JobController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/jobs")
+@RequestMapping("api/jobs")
 @RequiredArgsConstructor
 public class JobController {
 
